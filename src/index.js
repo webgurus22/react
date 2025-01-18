708e1dbf2d883bbd3bdaa817eb0ca6de92ffe6fa
Harsh Thakur <07harshthakur@gmail.com>
2025-01-18 12:03:26 +0530

    harsh 18-01-2025

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.css";  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
